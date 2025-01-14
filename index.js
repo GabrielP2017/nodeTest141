d7304f884f3b6610967b7cbe6c361d4cba93800f
LEEKANGMIN <zx78410@gmail.com>
2025-01-14 17:05:35 +0900

    비밀보호편

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ const express = require('express')
 const app = express()
 
 // 사용할 포트를 지정합니다. 여기서는 3000번 포트를 사용합니다.
-const port = 3000
+const port = 3000 // http://localhost/3000
 
-//몽고DB와 어플 연결결
+const { User } = require("./models/User");
+// const models = require("./models/User");
+// const User = models.User;
+//이걸 생략하는 것.
+const bodyParser = require("body-parser");
 
-mongoose.connect('mongodb+srv://adminLE:fuck391cc@cluster0.o7vdg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-).then(() => console.log("MONGODB 연결됨...")).catch(console.error);
+//바디파서 옵션
+app.use(bodyParser.urlencoded({extended: true})); // application/x-www-form-urlencoded 데이터를 가져옴
+app.use(bodyParser.json()); //application/json 데이터 가져옴
+// `body-parser`는 HTTP 요청 본문은 문자열 형태로 전달되는데 JavaScript 객체로 변환해야 쉽게 다룰 수 있어어
+//`req.body`에 객체 형태로 저장하여 데이터를 쉽게 다룰 수 있게 합니다.
+
+//몽고DB와 어플 연결
+const mongoose = require("mongoose")
+const config = require("./config/key")
+
+mongoose.connect(config.mongoURI).then(() => console.log("MONGODB 연결됨...")).catch(console.error);
 // 써주는 이유 : 에러 때문에.
 //connect - MongoDB 데이터베이스에 연결을 시도합니다, then - MongoDB 연결에 성공하면 호출, catch - 연결이 실패하면 에러가 발생하며, 이 에러를 처리할 수 있습니다.
 
@@ -20,8 +33,25 @@ mongoose.connect('mongodb+srv://adminLE:fuck391cc@cluster0.o7vdg.mongodb.net/?re
 // GET 요청으로 '/' 경로에 접근했을 때 실행되는 라우터(핸들러)입니다.
 app.get('/', (req, res) => {
   // 클라이언트(브라우저)에게 'Hello World!' 문자열을 응답으로 보냅니다.
-  res.send('Hello World! 이것은 테스트 입니다.')
+  res.send('Hello World! 이것은 테스트 입니다. 추가 내용입니다.')
 })
+
+//강의하고 다른점
+app.post("/register", async (req, res) => {
+  //회원가입에 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어준다.
+try{
+  const user = new User(req.body) // body-parser를 이용해서 클라이언트 정보를 받아줌.
+  await user.save()
+  res.status(200).json({success:true})
+
+}catch(err){
+  res.status(500).json({success:false, error:err.message})
+
+}
+
+})
+//postman 관련
+
 
 // 지정한 포트에서 서버를 실행합니다.
 app.listen(port, () => {
